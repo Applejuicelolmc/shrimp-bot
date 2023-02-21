0426916ba257bd0ea3914741cbd28eeff1446e20
Applejuicelolmc <dekortbrian1@gmail.com>
2023-02-21 21:07:26 +0100

    Added new environment variables

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,11 @@
+// This ensures your .env variables are the right type and are available for typescript autocomplete
+
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			DISCORD_TOKEN: string;
+			CLIENT_ID: string;
+			DEV_GUILD_ID: string;
 		}
 	}
 }
