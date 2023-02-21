c3c3723a5e422caf5d4332d3c1636486b7f5bffd
Applejuicelolmc <dekortbrian1@gmail.com>
2023-02-21 21:09:33 +0100

    Added command handler

---
+++ src/handlers/commandHandler.ts
@@ -0,0 +1,128 @@
+import { readdir, stat } from "fs/promises";
+import { ShrimpCategory, ShrimpClient, ShrimpCommand, SlashData } from "../common/base";
+import { ContextMenuCommandBuilder, REST, Routes, SlashCommandBuilder } from "discord.js";
+import { info } from "winston";
+import { stdout } from "process";
+
+export default async function commandHandler(client: ShrimpClient): Promise<void> {
+	const { commands, categories, infoLogger, errorLogger, paths } = client;
+
+	const rest = new REST({
+		version: '10'
+	})
+	.setToken(process.env.DISCORD_TOKEN!);
+
+	async function fetchCommands(): Promise<SlashCommandBuilder[]> {
+		const slashCommands: SlashCommandBuilder[] = [];
+		const sortedCategories: ShrimpCategory[] = [];
+		const commandFolder =  await readdir(paths.commands);
+		
+		for (const category of commandFolder) {
+			const stats = await stat(`${paths.commands}/${category}`);
+	
+			if (stats.isFile()) {
+				continue; //ignore files when reading commands folder
+			}
+
+			const categoryFolder = await readdir(`${paths.commands}/${category}`);
+	
+			const commandFiles = categoryFolder.filter(file => file.endsWith('.ts'));
+	
+			const { description, position, emoji } = require(`${paths.commands}/${category}/info.json`);
+
+			sortedCategories.push({
+				name: category,
+				info: {
+					description: description,
+					position: position,
+					emoji: emoji,
+					commandNames: [],
+				}
+			});
+
+			for (const commandFile of commandFiles) {
+				const command = require(`${paths.commands}/${category}/${commandFile}`).default as ShrimpCommand;
+
+				commands.set(command.info.name, command);
+	
+				sortedCategories[sortedCategories.length - 1].info.commandNames.push(command.info.name);
+	
+				slashCommands.push(command.info);
+			}
+		}
+
+		sortedCategories.sort((a, b) => {
+			return a.info.position - b.info.position;
+		});
+
+		for (const category of sortedCategories) {
+			categories.set(category.name, category);
+		}
+
+		return slashCommands;
+	}
+
+	async function loadCommands(commands: Promise<SlashCommandBuilder[]>): Promise<void> {
+		const resolvedCommands: (SlashCommandBuilder | ContextMenuCommandBuilder)[] = await commands;
+
+		try {
+			// Register commands to dev server only
+			await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.DEV_GUILD_ID!), {
+				body: resolvedCommands
+			});
+
+			infoLogger.info( `Registered ${resolvedCommands.length} commands to the dev server`);
+
+			// Register commands to all joined servers
+			// await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+			// 	body: resolvedCommands
+			// });
+			//
+			// infoLogger.info( `Registered ${resolvedCommands.length} commands globally`);
+
+		} catch (error) {
+			if (error instanceof Error) {
+				errorLogger.error(`Failed to register commands: ${error.message}`);
+			} else {
+				errorLogger.error(`Failed to register commands: ${error}`);
+			}
+		}
+	}
+
+	async function resetCommands(): Promise<void> {
+		try {
+			infoLogger.info(`Resetting all commands...`);
+
+			await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+				body: []
+			});
+
+			await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.DEV_GUILD_ID!), {
+				body: []
+			});
+
+			infoLogger.info(`All commands have been reset!`);
+
+		} catch (error) {
+			if (error instanceof Error) {
+				errorLogger.error(`Failed to reset commands: ${error.message}`);
+			} else {
+				errorLogger.error(`Failed to reset commands: ${error}`);
+			}
+		}
+	}
+
+	if (process.argv.includes('reset')) {
+		await resetCommands();
+	}
+
+	try {
+		loadCommands(fetchCommands());
+	} catch (error) {
+		if (error instanceof Error) {
+			errorLogger.error(`Command handler: ${error.message}`);
+		} else {
+			errorLogger.error(`Command handler: ${error}`);
+		}
+	}
+}
