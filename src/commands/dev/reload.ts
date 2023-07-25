bcd5e10a03d3863a413eb64e5c09fcad0f60560b
Applejuicelolmc <dekortbrian1@gmail.com>
2023-07-25 11:14:30 +0200

    Added experimental dev commands

---
+++ src/commands/dev/reload.ts
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { ShrimpCommand } from '../../common/base';
+import { fetchCommands, loadCommands, resetCommands } from '../../handlers/commandHandler';
+
+export default <ShrimpCommand>{
+	async execute(client, interaction): Promise<void> {
+		try {
+			if (interaction.user.id !== '298054641705549844') {
+				return;
+			}
+
+			await interaction.deferReply({ ephemeral: true });
+
+			await resetCommands(client);
+
+			await loadCommands(client, fetchCommands(client));
+
+			await interaction.editReply('reloaded all commands!');
+		} catch (error) {
+			client.handleError('reload command', error as Error);
+		}
+	},
+
+	slash: new SlashCommandBuilder().setName('reload').setDescription('reload all commands'),
+};
