95f15399b95c519a069d1afd16fb634b9908273e
Applejuicelolmc <dekortbrian1@gmail.com>
2023-07-25 11:22:43 +0200

    Added some functions for getting settings from db

--- src/handlers/mongoDBHandler.ts
+++ src/handlers/mongoDBHandler.ts
@@ -23,4 +23,62 @@ export default async function DBHandler(client: ShrimpClient): Promise<void> {
 			errorLogger.error(`MongoDB: ${error}`);
 		}
 	}
+
+export async function generateDefaultSettings(guild: Guild): Promise<IGuildSettingsSchema> {
+	return await GuildSettings.create(<IGuildSettingsSchema>{
+		guildName: guild.name,
+		guildId: guild.id,
+		categories: {
+			general: {
+				name: 'General settings',
+				description: `Some basic settings`,
+				settings: {
+					embedColor: {
+						fullName: 'Embed color',
+						description: 'The color that each of my embeds will use for this server',
+						value: Colors.Aqua,
+					},
+					disabledCommands: {
+						fullName: 'Disabled commands',
+						description: 'Commands that are disabled in this server',
+						value: 'none',
+					},
+				},
+			},
+			logging: {
+				name: 'Logging settings',
+				description: `Settings for the logging/moderating of messages`,
+				settings: {
+					enabled: {
+						fullName: 'Logging enabled',
+						description: 'Whether or not logging of message edits/deletes is enabled in this server',
+						value: false,
+					},
+					logChannel: {
+						fullName: 'Log channel',
+						description: 'The Channel to be used for logging',
+						value: 'none',
+					},
+					editWebhook: {
+						fullName: 'Edit Webhook',
+						description: 'The webhook used for logging message edits',
+						value: 'none',
+					},
+					deleteWebhook: {
+						fullName: 'Delete Webhook',
+						description: 'The webhook used for logging message deletes',
+						value: 'none',
+					},
+				},
+			},
+		},
+	});
+}
+
+export async function updateDB(guild: Guild): Promise<void> {
+	//TODO Add function to update guildSettings in case new settings are added, keeping original settings as they were before
+
+	const oldGuildSettings = (await GuildSettings.findOne({ guildId: guild.id })) as IGuildSettingsSchema;
+
+	console.log(oldGuildSettings.categories.general);
 }
