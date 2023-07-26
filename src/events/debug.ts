0815931e325ac622852bb88a48b10b3a70198ebd
Applejuicelolmc <dekortbrian1@gmail.com>
2023-07-26 03:31:49 +0200

    Added a debug event

---
+++ src/events/debug.ts
@@ -0,0 +1,20 @@
+import { Events } from 'discord.js';
+import { ShrimpEvent } from '../common/base';
+
+export default <ShrimpEvent>{
+	name: Events.Debug,
+	once: false,
+	async execute(client, debugMessage: string) {
+		const { debugLogger } = client;
+
+		if (debugMessage.includes('Heartbeat acknowledged')) {
+			return; // Ignore Heartbeat related debug messages as they are useless for now
+		}
+
+		const messageArray = debugMessage.split(/\n/g); // Split message on each new line
+
+		for (const message of messageArray) {
+			debugLogger.info(`${message}`);
+		}
+	},
+};
