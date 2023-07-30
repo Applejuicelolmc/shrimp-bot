fcaf17b51844821c30d6d066b8ce34d3207220a1
Applejuicelolmc <dekortbrian1@gmail.com>
2023-07-30 03:32:31 +0200

    Moved process events to a separate function

--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -41,71 +41,77 @@ export default async function eventHandler(client: ShrimpClient): Promise<void> 
 		}
 	}
 
-	try {
-		process.on('SIGINT', () => {
-			try {
-				process.exit(0);
-			} catch (error) {
-				client.handleError('SIGINT event', error as Error);
-			}
-		});
+	function runProcessEvents(): void {
+		try {
+			process.on('SIGINT', () => {
+				try {
+					process.exit(0);
+				} catch (error) {
+					client.handleError('SIGINT event', error as Error);
+				}
+			});
 
-		process.on('SIGTERM', () => {
-			try {
-				client.infoLogger.info('SIGTERM Received!');
-			} catch (error) {
-				client.handleError('SIGTERM event', error as Error);
-			}
-		});
+			process.on('SIGTERM', () => {
+				try {
+					client.infoLogger.info('SIGTERM Received!');
+				} catch (error) {
+					client.handleError('SIGTERM event', error as Error);
+				}
+			});
 
-		process.on('SIGQUIT', () => {
-			try {
-				client.infoLogger.info('SIGQUIT Received!');
-			} catch (error) {
-				client.handleError('SIGQUIT event', error as Error);
-			}
-		});
+			process.on('SIGQUIT', () => {
+				try {
+					client.infoLogger.info('SIGQUIT Received!');
+				} catch (error) {
+					client.handleError('SIGQUIT event', error as Error);
+				}
+			});
 
-		process.on('warning', (warning) => {
-			//Ignores warnings like a boss (I don't like messy terminal :p)
-			try {
-				if (warning.stack) {
-					const messageArray = warning.stack.split(/\n/g);
-					return;
-					for (const message of messageArray) {
-						client.warningLogger.info(`${message}`);
+			process.on('warning', (warning) => {
+				try {
+					if (warning.stack) {
+						const messageArray = warning.stack.split(/\n/g);
+
+						for (const message of messageArray) {
+							client.warningLogger.info(`${message}`);
+						}
 					}
+
+					client.warningLogger.info(`${warning.message}`);
+				} catch (error) {
+					client.handleError('Warning event', error as Error);
 				}
-				return;
-				client.warningLogger.info(`${warning.message}`);
-			} catch (error) {
-				client.handleError('Warning event', error as Error);
-			}
-		});
+			});
 
-		process.on('exit', (exitCode) => {
-			try {
-				sleep(5000); //wait 5s so the logger doesn't break xD, should fix this in a better way
-				client.destroy();
+			process.on('exit', (exitCode) => {
+				try {
+					client.destroy();
 
-				if (exitCode === 0) {
-					return client.infoLogger.info('Logged off');
-				} else {
-					return client.infoLogger.error(`Logged off with exit code ${exitCode}`);
+					if (exitCode === 0) {
+						return client.infoLogger.info('Logged off');
+					} else {
+						return client.infoLogger.error(`Logged off with exit code ${exitCode}`);
+					}
+				} catch (error) {
+					client.handleError('Exit event', error as Error);
 				}
-			} catch (error) {
-				client.handleError('Exit event', error as Error);
-			}
-		});
-		process.on('uncaughtException', (uncaughtException) => {
-			try {
-				client.handleError('Uncaught Exception', uncaughtException);
-			} catch (error) {
-				client.handleError('uncaughtException event', error as Error);
-			}
-		});
+			});
 
-		loadEvents(await fetchEvents());
+			process.on('uncaughtException', (uncaughtException) => {
+				try {
+					client.handleError('Uncaught Exception', uncaughtException);
+				} catch (error) {
+					client.handleError('uncaughtException event', error as Error);
+				}
+			});
+		} catch (error) {
+			client.handleError('Process event', error as Error);
+		}
+	}
+
+	try {
+		runProcessEvents();
+		await loadEvents(await fetchEvents());
 	} catch (error) {
 		client.handleError('Event handler', error as Error);
 	}
