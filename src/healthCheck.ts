d57312b084154029552c0436007b9645ba97ca82
Applejuicelolmc <dekortbrian1@gmail.com>
2024-01-27 02:29:46 +0100

    Updated healthchecks

--- src/healthCheck.ts
+++ src/healthCheck.ts
@@ -6,11 +6,14 @@ switch (client.ws.status) {
 	case Status.Ready:
 	case Status.Resuming:
 	case Status.Connecting:
+	case Status.Identifying:
 	case Status.Reconnecting:
+	case Status.WaitingForGuilds:
+	case Status.Nearly:
+		client.infoLogger.info(`Docker health: Client Healthy`);
 		process.exit(0);
-	case Status.Disconnected:
-		process.exit(1);
 
-	default:
+	case Status.Disconnected:
+		client.infoLogger.info(`Docker health: Client disconnected`);
 		process.exit(1);
 }
