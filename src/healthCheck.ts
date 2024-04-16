c198c087891301551d3cdf81748a7ac51ca2497c
Applejuicelolmc <dekortbrian1@gmail.com>
2024-04-16 02:28:25 +0200

    Added some console.logs in healthCheck.ts in case the client isn't ready yet

--- src/healthCheck.ts
+++ src/healthCheck.ts
@@ -12,19 +12,40 @@ switch (client.ws.status) {
 	case Status.Reconnecting:
 	case Status.WaitingForGuilds:
 	case Status.Nearly:
-		client.infoLogger.info(`Docker health: Client Healthy`);
+		if (client.user) {
+			client.infoLogger.info(`Docker health: Client Healthy`);
+		} else {
+			console.log(`Docker health: Client Healthy`);
+		}
+
 		exitCode = 0;
 		break;
 
 	case Status.Disconnected:
-		client.infoLogger.info(`Docker health: Client disconnected`);
+		if (client.user) {
+			client.infoLogger.info(`Docker health: Client disconnected`);
+		} else {
+			console.log(`Docker health: Client disconnected`);
+		}
+
 		exitCode = 1;
 		break;
 
 	default:
-		client.infoLogger.info(`Docker health: Client unhealthy`);
+		if (client.user) {
+			client.infoLogger.info(`Docker health: Client unhealthy`);
+		} else {
+			console.log(`Docker health: Client unhealthy`);
+		}
+
 		exitCode = 1;
 		break;
+}
+
+if (client.user) {
+	client.infoLogger.info(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
+} else {
+	console.log(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
 }
 
 process.exit(exitCode);
