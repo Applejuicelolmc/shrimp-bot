4c8c359877ceed7628b94f23044a81bbb420fad1
Applejuicelolmc <dekortbrian1@gmail.com>
2024-06-17 04:18:56 +0200

    you suck ass until you don't

--- src/healthCheck.ts
+++ src/healthCheck.ts
@@ -3,51 +3,54 @@ import { client } from './index.js';
 
 let exitCode: number;
 
-console.log(client.ws.status);
+try {
+	switch (client.ws.status) {
+		case Status.Idle:
+		case Status.Ready:
+		case Status.Resuming:
+		case Status.Connecting:
+		case Status.Identifying:
+		case Status.Reconnecting:
+		case Status.WaitingForGuilds:
+		case Status.Nearly:
+			if (client.user) {
+				client.infoLogger.info(`Docker health: Client Healthy`);
+			} else {
+				console.log(`Docker health: Client Healthy`);
+			}
 
-switch (client.ws.status) {
-	case Status.Idle:
-	case Status.Ready:
-	case Status.Resuming:
-	case Status.Connecting:
-	case Status.Identifying:
-	case Status.Reconnecting:
-	case Status.WaitingForGuilds:
-	case Status.Nearly:
-		if (client.user) {
-			client.infoLogger.info(`Docker health: Client Healthy`);
-		} else {
-			console.log(`Docker health: Client Healthy`);
-		}
+			exitCode = 0;
+			break;
 
-		exitCode = 0;
-		break;
+		case Status.Disconnected:
+			if (client.user) {
+				client.infoLogger.info(`Docker health: Client disconnected`);
+			} else {
+				console.log(`Docker health: Client disconnected`);
+			}
 
-	case Status.Disconnected:
-		if (client.user) {
-			client.infoLogger.info(`Docker health: Client disconnected`);
-		} else {
-			console.log(`Docker health: Client disconnected`);
-		}
+			exitCode = 1;
+			break;
 
-		exitCode = 1;
-		break;
+		default:
+			if (client.user) {
+				client.infoLogger.info(`Docker health: Client unhealthy`);
+			} else {
+				console.log(`Docker health: Client unhealthy`);
+			}
 
-	default:
-		if (client.user) {
-			client.infoLogger.info(`Docker health: Client unhealthy`);
-		} else {
-			console.log(`Docker health: Client unhealthy`);
-		}
+			exitCode = 1;
+			break;
+	}
 
-		exitCode = 1;
-		break;
+	if (client.user) {
+		client.infoLogger.info(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
+	} else {
+		console.log(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
+	}
+
+	process.exit(exitCode);
+} catch (error) {
+	client.handleError('Nuh uh', error as Error);
+	process.exit(1);
 }
-
-if (client.user) {
-	client.infoLogger.info(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
-} else {
-	console.log(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
-}
-
-process.exit(exitCode);
