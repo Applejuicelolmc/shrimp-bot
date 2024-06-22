cb0f4e09f6597c25686b84c091b19b30f691cf67
Applejuicelolmc <dekortbrian1@gmail.com>
2024-06-22 14:40:33 +0200

    Updated healthCheck command

--- src/healthCheck.ts
+++ src/healthCheck.ts
@@ -13,44 +13,26 @@ try {
 		case Status.Reconnecting:
 		case Status.WaitingForGuilds:
 		case Status.Nearly:
-			if (client.user) {
-				client.infoLogger.info(`Docker health: Client Healthy`);
-			} else {
-				console.log(`Docker health: Client Healthy`);
-			}
+			console.log(`Docker health: Client Healthy`);
 
 			exitCode = 0;
 			break;
 
 		case Status.Disconnected:
-			if (client.user) {
-				client.infoLogger.info(`Docker health: Client disconnected`);
-			} else {
-				console.log(`Docker health: Client disconnected`);
-			}
+			console.log(`Docker health: Client disconnected`);
 
 			exitCode = 1;
 			break;
 
 		default:
-			if (client.user) {
-				client.infoLogger.info(`Docker health: Client unhealthy`);
-			} else {
-				console.log(`Docker health: Client unhealthy`);
-			}
+			console.log(`Docker health: Client unhealthy`);
 
 			exitCode = 1;
 			break;
 	}
 
-	if (client.user) {
-		client.infoLogger.info(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
-	} else {
-		console.log(`Docker health: Client status: ${client.ws.status}, process exit code: ${exitCode}`);
-	}
-
 	process.exit(exitCode);
 } catch (error) {
-	client.handleError('Nuh uh', error as Error);
+	console.log('error: ', error);
 	process.exit(1);
 }
