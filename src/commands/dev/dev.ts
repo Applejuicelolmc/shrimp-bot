4916701c6c60a8c699eab351d6f36d57d75d395a
Applejuicelolmc <dekortbrian1@gmail.com>
2024-07-16 06:05:28 +0200

    Added an autocomplete interaction test

--- src/commands/dev/dev.ts
+++ src/commands/dev/dev.ts
@@ -1,9 +1,60 @@
-import { ActivityType, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ActivityType, ChatInputCommandInteraction, ColorResolvable, Colors, GuildBasedChannel, SlashCommandBuilder } from 'discord.js';
 import { ShrimpCommand } from '../../common/base.js';
 import { fetchCommands, loadCommands, resetCommands } from '../../handlers/commandHandler.js';
-import { testGuild, updateDB } from '../../handlers/mongoDBHandler.js';
+import { emdedColorUpdateSetting, settingPaths, testGuild, updateDB } from '../../handlers/mongoDBHandler.js';
 
 export default <ShrimpCommand>{
+	async autocomplete(client, interaction) {
+		const focusedValue = interaction.options.getFocused(true);
+		let choices: string[] = [];
+
+		console.log('focusedValue', focusedValue);
+
+		const colors = {
+			choices: Object.keys(Colors).map((color) => color),
+			values: [],
+		};
+
+		const commands = {
+			choices: client.commands.map((command) => command.slash.name),
+			values: [],
+		};
+
+		const channels = {
+			choices: interaction.guild?.channels.cache.map((channel) => channel.name) ?? [],
+			values: [],
+		};
+
+		switch (focusedValue.name) {
+			case 'embed-color':
+				choices = colors.choices.length < 25 ? colors.choices : colors.choices.slice(0, 25);
+
+				break;
+
+			case 'disabled-commands':
+				choices = commands.choices;
+
+				break;
+
+			case 'logging':
+				choices = ['true', 'false'];
+
+				break;
+
+			case 'log-channel':
+				choices = channels.choices.length < 25 ? channels.choices : channels.choices.slice(0, 25);
+
+				break;
+
+			default:
+				choices = ['nuh uh'];
+				break;
+		}
+
+		const filtered = choices.filter((choice) => choice.toLowerCase().includes(focusedValue.value));
+		await interaction.respond(filtered.map((choice) => ({ name: choice, value: choice })));
+	},
+
 	async execute(client, interaction: ChatInputCommandInteraction): Promise<void> {
 		if (interaction.isCommand()) {
 			async function reload() {
@@ -22,7 +73,7 @@ export default <ShrimpCommand>{
 
 			async function testDB() {
 				try {
-					await updateDB(testGuild, null, true);
+					await updateDB(testGuild, settingPaths.embedColor, Colors.DarkOrange);
 
 					client.getGuildSettings;
 
@@ -94,7 +145,14 @@ export default <ShrimpCommand>{
 		.setName('dev')
 		.setDescription('Run developer commands (dev only)')
 		.addSubcommand((subcommand) => subcommand.setName('reload').setDescription('Reload all commands'))
-		.addSubcommand((subcommand) => subcommand.setName('testdb').setDescription('Test the database'))
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName('testdb')
+				.setDescription('Test the database')
+				.addStringOption((embedColor) => embedColor.setName('embed-color').setDescription('embed color').setAutocomplete(true))
+				.addStringOption((disabledCommands) => disabledCommands.setName('disabled-commands').setDescription('disable commands').setAutocomplete(true))
+				.addStringOption((logChannel) => logChannel.setName('log-channel').setDescription('logChannel').setAutocomplete(true))
+		)
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('presence')
